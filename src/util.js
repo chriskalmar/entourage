4ed2dfda18e847fcf456c547f96a22ffee249bda
Chris Kalmar <christian.kalmar@gmail.com>
2019-11-15 03:02:11 +0100

    check if version folder is locked

--- src/util.js
+++ src/util.js
@@ -1,14 +1,29 @@
 import fs from 'fs';
 import path from 'path';
 
+export const createWorkPathFolder = () => {
+  if (!fs.existsSync(path.basename(process.env.WORK_PATH))) {
+    fs.mkdirSync(path.basename(process.env.WORK_PATH), { recursive: true });
+  }
+};
+
 export const lockWorkVersionFolder = version => {
   const filename = `${path.basename(process.env.WORK_PATH)}/${version}/.lock`;
   fs.writeFileSync(filename, '', 'utf8');
 };
 
-export const createWorkPathFolder = () => {
-  if (!fs.existsSync(path.basename(process.env.WORK_PATH))) {
-    fs.mkdirSync(path.basename(process.env.WORK_PATH), { recursive: true });
+export const isWorkVersionFolderLocked = version => {
+  const filename = `${path.basename(process.env.WORK_PATH)}/${version}/.lock`;
+  return fs.existsSync(filename);
+};
+
+export const deleteWorkVersionFolder = version => {
+  checkVersionPathBreakout(version);
+
+  const folderPath = `${path.basename(process.env.WORK_PATH)}/${version}`;
+
+  if (fs.existsSync(folderPath)) {
+    // fs.rmdirSync(folderPath, { recursive: true });
   }
 };
 
