36f1c7050890dae9b6aedd80ffd2e6332741d9b8
Chris Kalmar <christian.kalmar@gmail.com>
2019-11-26 00:40:05 +0100

    clean up stats module

--- src/stats.js
+++ src/stats.js
@@ -1,25 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import { renderFile } from './render';
-import { parseYaml } from './yaml';
-import { renderTemplateToFile } from './template';
-import {
-  createOrResetWorkVersionFolder,
-  lockWorkVersionFolder,
-  printTask,
-  log,
-  storeWorkVersionConfig,
-  getWorkVersionFolder,
-} from './util';
+import { log, getWorkVersionFolder } from './util';
 import { executeScript } from './script';
-import {
-  processDockerTask,
-  pullForDockerComposeFile,
-  runWorkVersionDockerComposeFile,
-  getDockerComposeStats,
-} from './docker';
-import { updateProxyConfig, restartProxy } from './proxy';
-import { addWorkVersionConfig, getWorkVersionConfig } from './registry';
+import { getDockerComposeStats } from './docker';
+import { getWorkVersionConfig } from './registry';
 
 export const getProfileStats = async (profile, version) => {
   const config = getWorkVersionConfig({ profile, version });
