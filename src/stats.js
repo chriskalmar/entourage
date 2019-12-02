c30a4f0dfcfa023ef26be541310ba7ef5a29f102
Chris Kalmar <christian.kalmar@gmail.com>
2019-12-02 17:54:40 +0100

    check if profile was found before getting stats

--- src/stats.js
+++ src/stats.js
@@ -6,6 +6,10 @@ import { getWorkVersionConfig } from './registry';
 export const getProfileStats = async (profile, version) => {
   const config = getWorkVersionConfig({ profile, version });
 
+  if (!config) {
+    throw new Error('Unknown profile')
+  }
+
   const filePath = config.docker.composeFile;
   const workVersionFolder = getWorkVersionFolder(version);
   const stats = await getDockerComposeStats(workVersionFolder, filePath);
