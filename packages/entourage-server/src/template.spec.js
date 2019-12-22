34696e62d3e341b77b95c239530caeb6868ed5f0
Chris Kalmar <christian.kalmar@gmail.com>
2019-12-23 00:59:35 +0100

    update tests

--- packages/entourage-server/src/template.spec.js
+++ packages/entourage-server/src/template.spec.js
@@ -2,7 +2,7 @@ import { renderTemplate } from './template';
 
 describe('tempalte', () => {
   it('should render from a template', () => {
-    const result = renderTemplate('../test/demo-profile.yaml', {
+    const result = renderTemplate(`${__dirname}/../test/demo-profile.yaml`, {
       __PROFILE: 'blue-space-omlette',
     });
 
